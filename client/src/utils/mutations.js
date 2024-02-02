be6776a00371646c610720dba9cff2de74e8ee3f
Jacob <jshairfield13@gmail.com>
2024-02-01 19:51:11 -0500

    mutations and queries no actual code yet but basic needs met

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+// mutations
+
+// create the user/ signup
+export const CREATE_USER = gql``;
+// login user
+export const LOGIN_USER = gql``;
+// edit user info
+export const EDIT_USER = gql``;
+// add a match to matches array?
+export const ADD_MATCH = gql``;
+// delete match from matches array
+export const REMOVE_MATCH = gql``;
+
+// instant messaging function??? for now leave comments on a page that only the author and page owner can see. a back and forth comment box.... 
+export const ADD_IM =gql``
+
+export const REMOVE_IM =gql``
+
+// potential donations angle??
