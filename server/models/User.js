ff5485a87b7dce26fc213f2eb4fd520f7e71e504
Gatsbeau <jabriel0ne@gmail.com>
2024-02-05 19:15:15 -0500

    Added bcrypt method to User model; implemented text search input component

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   firstName: {
@@ -61,4 +62,17 @@ const userSchema = new Schema({
   },
 });
 
-module.exports = userSchema;
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
