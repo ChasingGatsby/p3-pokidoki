43e0a994df688a1a6589e24e30f1f246800c67e1
Jacob <jshairfield13@gmail.com>
2024-02-12 19:39:50 -0500

    conditional rendering of login/logout

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,51 @@
 import logo from "../images/logo.png";
+import Auth from "../utils/auth";
 
 const NavBar = () => {
+  const handleLogout = () => {
+    Auth.logout();
+  };
+
+  if (!Auth.loggedIn()) {
+    return (
+      <header>
+        <a href="/">
+          <img className="logo" src={logo} alt="PokiDoki" />
+        </a>
+        <form>
+          <div className="search">
+            <span className="search-icon material-symbols-outlined">
+              search
+            </span>
+            <input
+              className="search-input"
+              type="search"
+              placeholder="Search..."
+            />
+          </div>
+        </form>
+        <nav>
+          <ul className="nav_links">
+            <li>
+              <a href="/profile">Profile</a>
+            </li>
+            <li>
+              <a href="/settings">Settings</a>
+            </li>
+            <li>
+              <a href="/search">Browse</a>
+            </li>
+            <li>
+              <a href="/matches">Matches</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
   return (
     <header>
       <a href="/">
@@ -31,7 +76,7 @@ const NavBar = () => {
             <a href="/matches">Matches</a>
           </li>
           <li>
-            <a href="/logout">Logout</a>
+            <button onClick={handleLogout}>Logout</button>
           </li>
         </ul>
       </nav>
