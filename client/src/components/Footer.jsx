aa900365690ade36c3d46b3f2f7a3eae68c27e90
Gatsbeau <jabriel0ne@gmail.com>
2024-02-13 20:25:53 -0500

    Styling changes; changes to match queries

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,6 @@ import { MdCatchingPokemon } from "react-icons/md";
 import { useContext } from "react";
 import ThemeContext from "../utils/themeContext";
 
-
 const themeStyles = {
   "Poke Ball": "red",
   "Great Ball": "blue",
@@ -13,19 +12,26 @@ const themeStyles = {
 export default function Footer() {
   const { theme } = useContext(ThemeContext);
   return (
-    
     <footer
       className="footer"
       style={{
         backgroundColor: themeStyles[theme],
         borderTop: "2px solid black",
+        maxWidth: "100vw",
       }}
     >
       <MdCatchingPokemon size={20} />
       <label className="mx-3" style={{ fontSize: "15px" }}>
-        Disclaimer
-      </label>
+       Disclaimer:
+      </label>{" "}
       <MdCatchingPokemon size={20} />
+      <label style={{ fontSize: "9px" }}>
+        This application is a fan-made project and is not affiliated with the
+        official Pokémon brand, The Pokémon Company, or Nintendo. All Pokémon
+        content, including images, names, and information, are owned by The
+        Pokémon Company and Nintendo. This application uses these assets for the
+        purpose of fan enjoyment and is not intended for commercial use.
+      </label>
     </footer>
   );
 }
